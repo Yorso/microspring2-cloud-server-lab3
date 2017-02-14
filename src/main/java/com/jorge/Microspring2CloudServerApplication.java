2f9d5b06fe5c412559eae69c5ffb5eff2a5036dd
Yorso <jorgeguzon@gmail.com>
2017-02-14 13:19:04 +0100

    changes

--- src/main/java/com/jorge/Microspring2CloudServerApplication.java
+++ src/main/java/com/jorge/Microspring2CloudServerApplication.java
@@ -12,7 +12,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 /**
  * What happens if the Config Server is unavailable when the “lucky word” application starts? To mitigate this possibility, it is common to run multiple instances of the config server in different racks / zones behind a load balancer.
  * What happens if we change a property after client applications have started? The server picks up the changes immediately, but the client does not. Later we will see how Spring Cloud Bus and refresh scope can be used to dynamically propagate changes.
- *
+ * 
  */
 @SpringBootApplication
 @EnableConfigServer
